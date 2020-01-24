a2d4dd1e5a3dd92b5e185dc9ac2755b8b0fd3cbf
Ricard Torres <ricard.torres@netcentric.biz>
2020-01-24 08:16:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,51 +9,89 @@
 <body>
 <?php
 
-https://www.goodreads.com/api
-include ('api.php');
+//======================================================================
+// Define shelves and order of rendering
+//======================================================================
 
-$shelves = ['currently-reading', 'to-read', 'read'];
+$shelves_ids = ['currently-reading', 'to-read', 'read'];
 
-$profile_rss = 'https://www.goodreads.com/review/list_rss/104159625?key=&shelf=';
+// To be used as section title output
+$shelves_title = ['📖 Currently reading', '📚 Want to read', '✅ Read'];
 
-foreach ($shelves as $shelf) {
+//======================================================================
+// Define your user profile
+//======================================================================
+
+$user_profile_id = "104159625";
+
+//======================================================================
+// RSS lists
+//======================================================================
+// Note: it seems API key might not be necessary if you're logged in on the site.
+$profile_rss = 'https://www.goodreads.com/review/list_rss/' . $user_profile_id .'?key=&shelf=';
+
+//======================================================================
+// The danger zone
+//======================================================================
+$i = 0;
+foreach ($shelves_ids as $shelf) {
+  // Construct the shelf feed and fetch it
   $feed = simplexml_load_file($profile_rss.$shelf);
 
-    echo "<h1>" . $shelf . "</h1>";
+  // Print the shelf heading
+  echo "<h2>" . $shelves_title[$i] . "</h2>";
 
-    $years = [];
+  // We will group the books by year on each shelf
+  $years = [];
 
-    foreach ($feed->channel->item as $item) {
-      $years[date('Y', strtotime($item->pubDate))][] = $item;
+  // Create the year groups
+  foreach ($feed->channel->item as $item) {
+    $years[date('Y', strtotime($item->pubDate))][] = $item;
+  }
+
+  foreach ($years as $key => $year) {
+    // On a personal preference I only separate by year
+    // the read books. The others go together, even if I have added them
+    // during different years.
+    if ($shelf === 'read') {
+      echo "<h3>" . $key . "</h3>";
     }
 
-    foreach ($years as $key => $year) {
-      if ($shelf === 'read') {
-        echo "<h3>" . $key . "</h3>";
-      }
+    // Create the list markup
+    echo "<ul>";
 
-      echo "<ul>";
-      foreach ($year as $item) {
-        $link = $item->link;
-        $rating = $item->user_rating;
-        ?>
-        <li><a href="<?=$link?>" rel="nofollow" title="Goodreads: <?=$item->title?>"><?=$item->title?> <?php
-            if ($shelf === 'read' && $rating) {
-              for ($i=0; $i < intval($rating); $i++) {
-                echo "⭐️";
-              }
-            } ?></a>
-        </li>
+    foreach ($year as $item) {
+      // Access the RSS object
+      $link = $item->link;
+      $rating = $item->user_rating;
+      $title = $item->title;
+    ?>
+      <li><?php
+        echo '<a href="' .$link . '" rel="nofollow" title="Goodreads: ' . $title . '">'. $title .' ';
+
+        // Output the rating with stars emoji, only for the read books
+        if ($shelf === 'read' && $rating) {
+          for ($i=0; $i < intval($rating); $i++) {
+            echo "⭐️";
+          }
+        }
+
+        echo '</a>';
+      ?></li>
       <?php
       } // end item loop
+
+      // I read in the API docs to not do more than 1 request per second
+      // Just in case, we'll wait 1 second in between shelves.
       sleep(1);
     ?>
     </ul>
     <?php
-      } // end year loop
+     } // end year loop
+
+  $i++;
 } // end foreach shelves
 
 ?>
-
 </body>
 </html>
